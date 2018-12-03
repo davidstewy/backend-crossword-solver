74ea05017498fc0d17bdeaa61a515e40d401f863
David Stewart <davidstewy@gmail.com>
2018-12-03 11:47:44 -0500

    finally got it

--- xword.py
+++ xword.py
@@ -2,20 +2,30 @@
 # -*- coding: utf-8 -*-
 """Crossword Solver Program"""
 
-__author__ = "???"
+__author__ = "davidstewy"
 
-# YOUR HELPER FUNCTION GOES HERE
+import re
+
+
+def word_find(input_word, word_list):
+    altered_word = input_word.replace(' ', '.')
+    regexed_word = re.compile(altered_word)
+    for word in word_list:
+        if len(input_word) == len(word):
+            if re.match(regexed_word, word):
+                print word
 
 
 def main():
+    words = []
     with open('dictionary.txt') as f:
         words = f.read().split()
 
     test_word = raw_input(
         'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
+    # raise NotImplementedError('Please complete this')
 
-    # YOUR ADDITIONAL CODE HERE
-    raise NotImplementedError('Please complete this')
+    word_find(test_word, words)
 
 
 if __name__ == '__main__':
